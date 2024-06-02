1202161454b9ae0166f5a930e57c8ad86f034e67
huiyaopuah <huiyao.puah@itoswtech.com.my>
2024-06-03 07:48:22 +0800

    Commit

--- src/context/player/PlayerContext.tsx
+++ src/context/player/PlayerContext.tsx
@@ -1,40 +1,40 @@
 import { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react";
 
 interface IPlayerType {
-  srno: number;
-  agentID: string;
-  playerID: string;
-  playerToken: string;
-  playerType: string;
-  wallet1: number;
-  playerName: string;
-  phoneNo: string;
-  email: string;
-  status: number;
-  createDate: Date;
+    srno: number;
+    agentID: string;
+    playerID: string;
+    playerToken: string;
+    playerType: string;
+    wallet1: number;
+    playerName: string;
+    phoneNo: string;
+    email: string;
+    status: number;
+    createDate: Date;
 }
 
 interface IPlayerContextType {
-  playerInfo: IPlayerType | undefined;
-  setPlayerInfo: Dispatch<SetStateAction<IPlayerType | undefined>>;
-  hostname: string;
+    playerInfo: IPlayerType | undefined;
+    setPlayerInfo: Dispatch<SetStateAction<IPlayerType | undefined>>;
+    hostname: string;
 }
 const initPlayerContext: IPlayerContextType = {
-  playerInfo: undefined,
-  setPlayerInfo: () => {},
-  hostname: "",
+    playerInfo: undefined,
+    setPlayerInfo: () => {},
+    hostname: "",
 };
 export const Player = createContext<IPlayerContextType>(initPlayerContext);
 
 interface IPlayerContextProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 const PlayerContext = ({ children }: IPlayerContextProps) => {
-  const [playerInfo, setPlayerInfo] = useState<IPlayerType | undefined>(undefined);
-  const hostname = window.location.hostname === "localhost" ? "test" : window.location.hostname;
+    const [playerInfo, setPlayerInfo] = useState<IPlayerType | undefined>(undefined);
+    const hostname = window.location.hostname === "localhost" ? "test" : window.location.hostname.split(".")[0];
 
-  const values: IPlayerContextType = { playerInfo, setPlayerInfo, hostname };
-  return <Player.Provider value={values}>{children}</Player.Provider>;
+    const values: IPlayerContextType = { playerInfo, setPlayerInfo, hostname };
+    return <Player.Provider value={values}>{children}</Player.Provider>;
 };
 
 export default PlayerContext;
