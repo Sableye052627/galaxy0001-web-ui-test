7533f9a06ab57e928d84da7f6ad1287d3eacca21
huiyaopuah <huiyao.puah@itoswtech.com.my>
2024-06-05 15:56:38 +0800

    Commit

--- src/service/CallApi.tsx
+++ src/service/CallApi.tsx
@@ -1,41 +1,46 @@
 import ApiBaseUrl from "./ApiBaseUrl";
 
-// const hostname = window.location.hostname;
-// const api1 = hostname === "localhost" ? "https://localhost:44303/api" : hostname === "webh5-bo.danger.asia" ? "webh5-bo-api.danger.asia" : `https://bo-api.${hostname.split("portal.")[1]}/api`;
+const hostname = window.location.hostname;
+const api1 =
+    hostname === "localhost"
+        ? "http://localhost:19013/api"
+        : hostname === "webh5.danger.asia"
+        ? "webh5-ui-api.danger.asia"
+        : `https://ui-api.${hostname}/api`;
 
-const api1 = process.env.REACT_APP_API_BASE_URL as string;
+// const api1 = process.env.REACT_APP_API_BASE_URL as string;
 
 export async function platformApi(url: string, object: object) {
-  const response = await ApiBaseUrl(api1).post("platform" + url, object);
-  const result = response.data;
+    const response = await ApiBaseUrl(api1).post("platform" + url, object);
+    const result = response.data;
 
-  return result;
+    return result;
 }
 
 export async function playerApi(url: string, object: object) {
-  const response = await ApiBaseUrl(api1).post("player" + url, object);
-  const result = response.data;
+    const response = await ApiBaseUrl(api1).post("player" + url, object);
+    const result = response.data;
 
-  return result;
+    return result;
 }
 
 export async function gameApi(url: string, object: object) {
-  const response = await ApiBaseUrl(api1).post("game" + url, object);
-  const result = response.data;
+    const response = await ApiBaseUrl(api1).post("game" + url, object);
+    const result = response.data;
 
-  return result;
+    return result;
 }
 
 export async function gameProviderApi(url: string, object: object) {
-  const response = await ApiBaseUrl(api1).post("game-provider" + url, object);
-  const result = response.data;
+    const response = await ApiBaseUrl(api1).post("game-provider" + url, object);
+    const result = response.data;
 
-  return result;
+    return result;
 }
 
 export async function theOneApi(url: string, object: object) {
-  const response = await ApiBaseUrl(api1).post("theone" + url, object);
-  const result = response.data;
+    const response = await ApiBaseUrl(api1).post("theone" + url, object);
+    const result = response.data;
 
-  return result;
+    return result;
 }
