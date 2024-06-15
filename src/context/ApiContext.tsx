f403895437b60427718f0ba4e65135b191f21d42
oneNasiLemak <khaisheng0811ksc@gmail.com>
2024-06-15 12:15:35 +0800

    commit

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -6,131 +6,131 @@ import { getHomePage, getPlatformInfo, validateToken } from "../function/ApiFunc
 import Loading from "../component/loading/Loading";
 
 interface IPlatformType {
-    srno: number;
-    uniqueID: string;
-    platformName: string;
-    accountPrefix: string;
-    primaryColor: string;
-    secondaryColor: string;
-    tabIcon: string;
-    logoImage: string;
-    logoImage_2: string;
-    backgroundColor: string;
-    textColor: string;
-    headerColor: string;
-    footerColor: string;
-    mode: string;
-    descriptionEN: string;
-    descriptionCN: string;
-    descriptionMY: string;
-    androidDownloadUrl: string;
-    iosDownloadUrl: string;
-    safePay: number;
-    safePayTelco: number;
-    safepPayOB: number;
-    safePayQR: number;
-    cdm: number;
-    cdM_Deposit: number;
-    cdM_Withdrawal: number;
-    suggestion: number;
-    selfRegister: number;
-    phoneNoVerify: number;
+  srno: number;
+  uniqueID: string;
+  platformName: string;
+  accountPrefix: string;
+  primaryColor: string;
+  secondaryColor: string;
+  tabIcon: string;
+  logoImage: string;
+  logoImage_2: string;
+  backgroundColor: string;
+  textColor: string;
+  headerColor: string;
+  footerColor: string;
+  mode: string;
+  descriptionEN: string;
+  descriptionCN: string;
+  descriptionMY: string;
+  androidDownloadUrl: string;
+  iosDownloadUrl: string;
+  safePay: number;
+  safePayTelco: number;
+  safepPayOB: number;
+  safePayQR: number;
+  cdm: number;
+  cdM_Deposit: number;
+  cdM_Withdrawal: number;
+  suggestion: number;
+  selfRegister: number;
+  phoneNoVerify: number;
 }
 interface IGpCategoryType {
-    category: string;
+  category: string;
 }
 
 interface IBannerList {
-    srno: number;
-    advertisingType: string;
-    title: string;
-    sort: number;
-    mediaUrl: string;
-    createDate: Date;
+  srno: number;
+  advertisingType: string;
+  title: string;
+  sort: number;
+  mediaUrl: string;
+  createDate: Date;
 }
 
 interface IGpList {
-    srno: number;
-    gameName: string;
-    gameCode: string;
-    type: string;
-    logoImage: string;
-    bannerImage: string;
-    btnImage: string;
-    status: number;
+  srno: number;
+  gameName: string;
+  gameCode: string;
+  type: string;
+  logoImage: string;
+  bannerImage: string;
+  btnImage: string;
+  status: number;
 }
 
 interface IApiContextType {
-    windowWidth: number;
-    setWindowWidth: Dispatch<SetStateAction<number>>;
-    platformInfo: IPlatformType | undefined;
-    setPlatformInfo: Dispatch<SetStateAction<IPlatformType | undefined>>;
-    gpCategory: IGpCategoryType[] | [];
-    setGpCategory: Dispatch<SetStateAction<IGpCategoryType[] | []>>;
-    bannerList: IBannerList[] | [];
-    setBannerList: Dispatch<SetStateAction<IBannerList[] | []>>;
-    gpList: IGpList[] | [];
-    setGpList: Dispatch<SetStateAction<IGpList[] | []>>;
+  windowWidth: number;
+  setWindowWidth: Dispatch<SetStateAction<number>>;
+  platformInfo: IPlatformType | undefined;
+  setPlatformInfo: Dispatch<SetStateAction<IPlatformType | undefined>>;
+  gpCategory: IGpCategoryType[] | [];
+  setGpCategory: Dispatch<SetStateAction<IGpCategoryType[] | []>>;
+  bannerList: IBannerList[] | [];
+  setBannerList: Dispatch<SetStateAction<IBannerList[] | []>>;
+  gpList: IGpList[] | [];
+  setGpList: Dispatch<SetStateAction<IGpList[] | []>>;
 }
 const initApiContext: IApiContextType = {
-    windowWidth: window.innerWidth,
-    setWindowWidth: () => {},
-    platformInfo: undefined,
-    setPlatformInfo: () => {},
-    gpCategory: [],
-    setGpCategory: () => {},
-    bannerList: [],
-    setBannerList: () => {},
-    gpList: [],
-    setGpList: () => {},
+  windowWidth: window.innerWidth,
+  setWindowWidth: () => {},
+  platformInfo: undefined,
+  setPlatformInfo: () => {},
+  gpCategory: [],
+  setGpCategory: () => {},
+  bannerList: [],
+  setBannerList: () => {},
+  gpList: [],
+  setGpList: () => {},
 };
 export const Api = createContext<IApiContextType>(initApiContext);
 
 interface IApiContextProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 const ApiContext = ({ children }: IApiContextProps) => {
-    const playerContext = useContext(Player);
-    const { setPlayerInfo, setAgentInfo, hostname } = playerContext;
+  const playerContext = useContext(Player);
+  const { setPlayerInfo, agentInfo, setAgentInfo, hostname } = playerContext;
 
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
 
-    const [platformInfo, setPlatformInfo] = useState<IPlatformType | undefined>(undefined);
-    const [gpCategory, setGpCategory] = useState<IGpCategoryType[] | []>([]);
-    const [bannerList, setBannerList] = useState<IBannerList[] | []>([]);
-    const [gpList, setGpList] = useState<IGpList[] | []>([]);
+  const [platformInfo, setPlatformInfo] = useState<IPlatformType | undefined>(undefined);
+  const [gpCategory, setGpCategory] = useState<IGpCategoryType[] | []>([]);
+  const [bannerList, setBannerList] = useState<IBannerList[] | []>([]);
+  const [gpList, setGpList] = useState<IGpList[] | []>([]);
 
-    useEffect(() => {
-        handleFirstLoad();
-    }, []);
+  useEffect(() => {
+    handleFirstLoad();
+  }, []);
 
-    async function handleFirstLoad() {
-        const api1 = validateToken(hostname, setPlayerInfo, setAgentInfo);
-        const api2 = getPlatformInfo(hostname, setPlatformInfo, setGpCategory);
-        const api3 = getHomePage(hostname, setBannerList, setGpList);
+  async function handleFirstLoad() {
+    const api1 = validateToken(hostname, setPlayerInfo, setAgentInfo);
+    const api2 = getPlatformInfo(hostname, setPlatformInfo, setGpCategory, setAgentInfo);
+    const api3 = getHomePage(hostname, setBannerList, setGpList);
 
-        await Promise.all([api1, api2, api3]).catch((error) => console.log(error));
-        setIsLoading(false);
-    }
+    await Promise.all([api1, api2, api3]).catch((error) => console.log(error));
+    setIsLoading(false);
+  }
 
-    if (isLoading) {
-        return <Loading />;
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    const values: IApiContextType = {
-        windowWidth,
-        setWindowWidth,
-        platformInfo,
-        setPlatformInfo,
-        gpCategory,
-        setGpCategory,
-        bannerList,
-        setBannerList,
-        gpList,
-        setGpList,
-    };
-    return <Api.Provider value={values}>{children}</Api.Provider>;
+  const values: IApiContextType = {
+    windowWidth,
+    setWindowWidth,
+    platformInfo,
+    setPlatformInfo,
+    gpCategory,
+    setGpCategory,
+    bannerList,
+    setBannerList,
+    gpList,
+    setGpList,
+  };
+  return <Api.Provider value={values}>{children}</Api.Provider>;
 };
 
 export default ApiContext;
--- src/function/ApiFunction.tsx
+++ src/function/ApiFunction.tsx
@@ -3,45 +3,42 @@ import { Dispatch, SetStateAction } from "react";
 import Cookies from "js-cookie";
 import { gameApi, platformApi, playerApi } from "../service/CallApi";
 
-export async function getPlatformInfo(
-    hostname: string,
-    setPlatformInfo: Dispatch<SetStateAction<any>>,
-    setGpCategory: Dispatch<SetStateAction<any>>
-) {
-    const object = { HostName: hostname };
-    await platformApi("/get-info", object).then((result) => {
-        setPlatformInfo(result.data);
-        setGpCategory(result.data2);
+export async function getPlatformInfo(hostname: string, setPlatformInfo: Dispatch<SetStateAction<any>>, setGpCategory: Dispatch<SetStateAction<any>>, setAgentInfo: Dispatch<SetStateAction<any>>) {
+  const object = { HostName: hostname };
+  await platformApi("/get-info", object).then((result) => {
+    setPlatformInfo(result.data);
+    setGpCategory(result.data2);
+    setAgentInfo((prev: any) => (prev ? prev : result.data3));
 
-        document.documentElement.style.setProperty("--primary-color", `#${result.data.primaryColor}`);
-        document.documentElement.style.setProperty("--secondary-color", `#${result.data.secondaryColor}`);
-        document.documentElement.style.setProperty("--background-color", `#${result.data.backgroundColor}`);
-        document.documentElement.style.setProperty("--text-color", `#${result.data.textColor}`);
-        document.documentElement.style.setProperty("--header-color", `#${result.data.headerColor}`);
-        document.documentElement.style.setProperty("--footer-color", `#${result.data.footerColor}`);
-        document.documentElement.setAttribute("data-theme", result.data.mode);
-    });
+    document.documentElement.style.setProperty("--primary-color", `#${result.data.primaryColor}`);
+    document.documentElement.style.setProperty("--secondary-color", `#${result.data.secondaryColor}`);
+    document.documentElement.style.setProperty("--background-color", `#${result.data.backgroundColor}`);
+    document.documentElement.style.setProperty("--text-color", `#${result.data.textColor}`);
+    document.documentElement.style.setProperty("--header-color", `#${result.data.headerColor}`);
+    document.documentElement.style.setProperty("--footer-color", `#${result.data.footerColor}`);
+    document.documentElement.setAttribute("data-theme", result.data.mode);
+  });
 }
 
 export async function getHomePage(hostname: string, setBannerList: Dispatch<SetStateAction<any>>, setGpList: Dispatch<SetStateAction<any>>) {
-    const object = { HostName: hostname };
-    await platformApi("/home-page", object).then((result) => {
-        setBannerList(result.data);
-        setGpList(result.data2);
-    });
+  const object = { HostName: hostname };
+  await platformApi("/home-page", object).then((result) => {
+    setBannerList(result.data);
+    setGpList(result.data2);
+  });
 }
 
 export async function validateToken(hostname: string, setPlayerInfo: Dispatch<SetStateAction<any>>, setAgentInfo: Dispatch<SetStateAction<any>>) {
-    const object = { HostName: hostname, PlayerID: Cookies.get("PlayerID"), PlayerToken: Cookies.get("PlayerToken") };
-    await playerApi("/validate-token", object).then((result) => {
-        setPlayerInfo(result.data);
-        setAgentInfo(result.data2);
-    });
+  const object = { HostName: hostname, PlayerID: Cookies.get("PlayerID"), PlayerToken: Cookies.get("PlayerToken") };
+  await playerApi("/validate-token", object).then((result) => {
+    setPlayerInfo(result.data);
+    setAgentInfo((prev: any) => (!result.data2.length ? prev : result.data2));
+  });
 }
 
 export async function getGameProviderList(hostname: string, setGpList: Dispatch<SetStateAction<any>>) {
-    const object = { HostName: hostname, PlayerID: Cookies.get("PlayerID"), PlayerToken: Cookies.get("PlayerToken") };
-    await gameApi("/game-provider-list", object).then((result) => {
-        setGpList(result.data);
-    });
+  const object = { HostName: hostname, PlayerID: Cookies.get("PlayerID"), PlayerToken: Cookies.get("PlayerToken") };
+  await gameApi("/game-provider-list", object).then((result) => {
+    setGpList(result.data);
+  });
 }
