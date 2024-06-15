8afdbcef699b42c84fd90e435d3542e06933b9e7
oneNasiLemak <khaisheng0811ksc@gmail.com>
2024-06-15 12:19:25 +0800

    commit

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -10,98 +10,99 @@ import Cookies from "js-cookie";
 import { LazyLoad } from "../loading/lazy-load/LazyLoad";
 
 const Login = () => {
-    const { t, i18n, navigate, platformInfo, windowWidth, playerInfo, setPlayerInfo, hostname } = useLogin();
+  const { t, i18n, navigate, platformInfo, windowWidth, playerInfo, setPlayerInfo, setAgentInfo, hostname } = useLogin();
 
-    const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    // const [banner, setBanner] = useState();
-    const [show, setShow] = useState<boolean>(false);
+  const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [banner, setBanner] = useState();
+  const [show, setShow] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (playerInfo) {
-            navigate("/");
-            return;
-        }
-        getBannerList();
-    }, []);
-
-    async function getBannerList() {
-        // const object = {
-        //     PlatformName: platformInfo?.platformName,
-        // };
-        // const result = await bannerApi("/banner-list", object);
-        // if (result.status) {
-        //     setBanner(result.data);
-        // }
-        setIsFirstLoad(false);
+  useEffect(() => {
+    if (playerInfo) {
+      navigate("/");
+      return;
     }
+    getBannerList();
+  }, []);
 
-    async function handleLogin(values: any) {
-        setIsLoading(true);
-        try {
-            const object = {
-                HostName: hostname,
-                PlayerID: values.playerID,
-                Password: values.password,
-            };
-            const result = await playerApi("/login", object);
-            if (result.status) {
-                setPlayerInfo(result.data);
+  async function getBannerList() {
+    // const object = {
+    //     PlatformName: platformInfo?.platformName,
+    // };
+    // const result = await bannerApi("/banner-list", object);
+    // if (result.status) {
+    //     setBanner(result.data);
+    // }
+    setIsFirstLoad(false);
+  }
 
-                Cookies.set("PlayerID", result.data.playerID);
-                Cookies.set("PlayerToken", result.data.playerToken);
-                i18n.changeLanguage(result.data.lang);
+  async function handleLogin(values: any) {
+    setIsLoading(true);
+    try {
+      const object = {
+        HostName: hostname,
+        PlayerID: values.playerID,
+        Password: values.password,
+      };
+      const result = await playerApi("/login", object);
+      if (result.status) {
+        setPlayerInfo(result.data);
+        setAgentInfo(result.data2);
 
-                setShow(true);
-            }
-        } catch (error: any) {
-            console.log(error);
-            // message.error({ content: error?.response?.data?.message, key: error?.response?.data?.message });
-        }
-        setIsLoading(false);
+        Cookies.set("PlayerID", result.data.playerID);
+        Cookies.set("PlayerToken", result.data.playerToken);
+        i18n.changeLanguage(result.data.lang);
+
+        setShow(true);
+      }
+    } catch (error: any) {
+      console.log(error);
+      // message.error({ content: error?.response?.data?.message, key: error?.response?.data?.message });
     }
+    setIsLoading(false);
+  }
 
-    if (isFirstLoad) {
-        return <LazyLoad />;
-    }
+  if (isFirstLoad) {
+    return <LazyLoad />;
+  }
 
-    return (
-        <Row className="login" justify="center">
-            <Col {...gridSetting}>
-                <SuccessModal show={show} setShow={setShow} />
+  return (
+    <Row className="login" justify="center">
+      <Col {...gridSetting}>
+        <SuccessModal show={show} setShow={setShow} />
 
-                <Row className="header">
-                    <Col xs={0} xl={24}>
-                        {t("login")}
-                    </Col>
-                </Row>
+        <Row className="header">
+          <Col xs={0} xl={24}>
+            {t("login")}
+          </Col>
+        </Row>
 
-                <Row className="content" justify="center" gutter={[16, 10]}>
-                    <Col xs={24} sm={16} md={14} lg={11} xl={8}>
-                        <Spin spinning={isLoading}>
-                            <Card>
-                                <Form layout="vertical" onFinish={handleLogin}>
-                                    <Form.Item label={t("playerID")} name="playerID" rules={[{ required: true, message: t("pleaseInsertPlayerID") }]}>
-                                        <Input />
-                                    </Form.Item>
+        <Row className="content" justify="center" gutter={[16, 10]}>
+          <Col xs={24} sm={16} md={14} lg={11} xl={8}>
+            <Spin spinning={isLoading}>
+              <Card>
+                <Form layout="vertical" onFinish={handleLogin}>
+                  <Form.Item label={t("playerID")} name="playerID" rules={[{ required: true, message: t("pleaseInsertPlayerID") }]}>
+                    <Input />
+                  </Form.Item>
 
-                                    <Form.Item label={t("password")} name="password" rules={[{ required: true, message: t("pleaseInsertPassword") }]}>
-                                        <Input.Password />
-                                    </Form.Item>
+                  <Form.Item label={t("password")} name="password" rules={[{ required: true, message: t("pleaseInsertPassword") }]}>
+                    <Input.Password />
+                  </Form.Item>
 
-                                    <Form.Item>
-                                        <Button block size="large" type="primary" htmlType="submit">
-                                            {t("login")}
-                                        </Button>
-                                    </Form.Item>
-                                </Form>
-                            </Card>
-                        </Spin>
-                    </Col>
+                  <Form.Item>
+                    <Button block size="large" type="primary" htmlType="submit">
+                      {t("login")}
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Card>
+            </Spin>
+          </Col>
 
-                    <Col xs={0} xl={16}>
-                        <Card>
-                            {/* <Carousel autoplay dots={false}>
+          <Col xs={0} xl={16}>
+            <Card>
+              {/* <Carousel autoplay dots={false}>
                                 {banner?.map((items: any, index: number) => (
                                     <div key={index} className="banner-img">
                                         <img src={items.image} alt={items.title} />
@@ -109,14 +110,14 @@ const Login = () => {
                                 ))}
                             </Carousel> */}
 
-                            <hr />
-                            <div className="description">{t("aboutUsDesc", { domainName: platformInfo?.platformName })}</div>
-                        </Card>
-                    </Col>
-                </Row>
-            </Col>
+              <hr />
+              <div className="description">{t("aboutUsDesc", { domainName: platformInfo?.platformName })}</div>
+            </Card>
+          </Col>
         </Row>
-    );
+      </Col>
+    </Row>
+  );
 };
 
 export default Login;
--- src/page/login/hook/useLogin.tsx
+++ src/page/login/hook/useLogin.tsx
@@ -5,14 +5,14 @@ import { Api } from "../../../context/ApiContext";
 import { Player } from "../../../context/player/PlayerContext";
 
 export const useLogin = () => {
-    const { t, i18n } = useTranslation();
-    const navigate = useNavigate();
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
 
-    const apiContext = useContext(Api);
-    const { platformInfo, windowWidth } = apiContext;
+  const apiContext = useContext(Api);
+  const { platformInfo, windowWidth } = apiContext;
 
-    const playerContext = useContext(Player);
-    const { playerInfo, setPlayerInfo, hostname } = playerContext;
+  const playerContext = useContext(Player);
+  const { playerInfo, setPlayerInfo, setAgentInfo, hostname } = playerContext;
 
-    return { t, i18n, navigate, platformInfo, windowWidth, playerInfo, setPlayerInfo, hostname };
+  return { t, i18n, navigate, platformInfo, windowWidth, playerInfo, setPlayerInfo, setAgentInfo, hostname };
 };
